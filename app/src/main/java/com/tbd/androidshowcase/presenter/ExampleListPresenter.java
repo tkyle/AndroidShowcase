eeaebabe8712def4dafee5fb1f4ce29d26993cdf
t-rev <trev.tkyle@gmail.com>
2016-09-04 16:32:51 -0500

    Added a few comments.

---
+++ app/src/main/java/com/tbd/androidshowcase/presenter/ExampleListPresenter.java
@@ -0,0 +1,15 @@
+package com.tbd.androidshowcase.presenter;
+
+import com.tbd.androidshowcase.view.IExampleListView;
+
+/**
+ * Created by Trevor on 9/4/2016.
+ */
+public class ExampleListPresenter
+{
+    IExampleListView view;
+
+    public void ExampleListPresenter(IExampleListView view) { this.view = view; }
+
+
+}
--- app/src/main/java/com/tbd/androidshowcase/ui/activity/ExampleListActivity.java
+++ app/src/main/java/com/tbd/androidshowcase/ui/activity/ExampleListActivity.java
@@ -4,12 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.tbd.androidshowcase.R;
+import com.tbd.androidshowcase.presenter.ExampleListPresenter;
+import com.tbd.androidshowcase.view.IExampleListView;
 
-public class ExampleListActivity extends AppCompatActivity {
+public class ExampleListActivity extends AppCompatActivity implements IExampleListView {
+
+    private ExampleListPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        presenter = new ExampleListPresenter(ExampleListActivity.this);
     }
 }
