71cd179efcc0c542b82802f6b746a2379b6a3258
t-rev <trev.tkyle@gmail.com>
2016-09-05 13:18:23 -0500

    Added a context menu when long pressing on a row item.

--- app/src/main/java/com/tbd/androidshowcase/ui/activity/ExampleListActivity.java
+++ app/src/main/java/com/tbd/androidshowcase/ui/activity/ExampleListActivity.java
@@ -2,20 +2,26 @@ package com.tbd.androidshowcase.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.tbd.androidshowcase.R;
+import com.tbd.androidshowcase.model.ExampleItem;
 import com.tbd.androidshowcase.presenter.ExampleListPresenter;
 import com.tbd.androidshowcase.view.IExampleListView;
+
+import java.util.ArrayList;
 
 public class ExampleListActivity extends AppCompatActivity implements IExampleListView {
 
     private ExampleListPresenter presenter;
     ArrayAdapter<String> listAdapter;
     ListView exampleListView;
+    ArrayList<String> exampleList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +30,26 @@ public class ExampleListActivity extends AppCompatActivity implements IExampleLi
 
         presenter = new ExampleListPresenter(ExampleListActivity.this);
 
-        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, presenter.GetExampleList());
+        exampleList = presenter.GetExampleList();
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, exampleList);
         //listAdapter = new ArrayAdapter<String>(this, R.layout.examplelist_row, presenter.GetExampleList());
 
         // Set the ArrayAdapter as the ListView's adapter.
         exampleListView = (ListView) findViewById( R.id.exampleListView );
         exampleListView.setAdapter( listAdapter );
-        exampleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        registerForContextMenu(exampleListView);
+    }
 
-            // TODO: Need to use example objects not strings
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //AddFriendToList(position);
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if (v.getId() == R.id.exampleListView) {
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            menu.setHeaderTitle(exampleList.get(info.position));
+
+            String[] menuItems = getResources().getStringArray(R.array.menu);
+            for (int i = 0; i < menuItems.length; i++) {
+                menu.add(Menu.NONE, i, i, menuItems[i]);
             }
-        });
+        }
     }
 }
