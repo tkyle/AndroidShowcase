1eff385353499761c295f38f4da6cd035004bf6e
t-rev <trev.tkyle@gmail.com>
2016-09-18 22:58:52 -0500

    modified the GetItems() in ProductTable class.

--- app/src/main/java/com/tbd/androidshowcase/tables/ProductTable.java
+++ app/src/main/java/com/tbd/androidshowcase/tables/ProductTable.java
@@ -25,12 +25,6 @@ public class ProductTable extends NoSQLTableBase
     private static final String LOG_TAG = ProductTable.class.getSimpleName();
     private final DynamoDBMapper mapper;
 
-    /** Inner classes use this value to determine how many results to retrieve per service call. */
-    private static final int RESULTS_PER_RESULT_GROUP = 40;
-
-    /** Removing sample data removes the items in batches of the following size. */
-    private static final int MAX_BATCH_SIZE_FOR_DELETE = 50;
-
     public ProductTable() {
         mapper = AWSMobileClient.defaultMobileClient().getDynamoDBMapper();
     }
@@ -54,8 +48,6 @@ public class ProductTable extends NoSQLTableBase
         newProduct.setDescription(product.getDescription());
         newProduct.setCost(product.getCost());
 
-        AmazonClientException lastException = null;
-
         try {
             mapper.save(newProduct);
             return newProduct;
@@ -71,7 +63,6 @@ public class ProductTable extends NoSQLTableBase
         Log.d(LOG_TAG, "Removing item from data.");
         final Product itemToFind = new Product();
 
-        // have to use Hash? That's why I set the ID, but I need to limit it more, so I'm using the range key?
         itemToFind.setUserId(AWSMobileClient.defaultMobileClient().getIdentityManager().getCachedUserID());
         final Condition rangeKeyCondition = new Condition().withComparisonOperator(ComparisonOperator.EQ.toString()).withAttributeValueList(new AttributeValue().withS(productId));
 
@@ -84,12 +75,9 @@ public class ProductTable extends NoSQLTableBase
 
         Iterator<Product> resultsIterator = results.iterator();
 
-        AmazonClientException lastException = null;
-
         if (resultsIterator.hasNext()) {
             final Product item = resultsIterator.next();
 
-            // Demonstrate deleting a single item.
             try {
                 mapper.delete(item);
             } catch (final AmazonClientException ex) {
@@ -107,7 +95,6 @@ public class ProductTable extends NoSQLTableBase
         Log.d(LOG_TAG, "edit item");
         final Product itemToFind = new Product();
 
-        // have to use Hash? That's why I set the ID, but I need to limit it more, so I'm using the range key?
         itemToFind.setUserId(AWSMobileClient.defaultMobileClient().getIdentityManager().getCachedUserID());
         final Condition rangeKeyCondition = new Condition().withComparisonOperator(ComparisonOperator.EQ.toString()).withAttributeValueList(new AttributeValue().withS(product.getProductId()));
 
@@ -120,21 +107,17 @@ public class ProductTable extends NoSQLTableBase
 
         Iterator<Product> resultsIterator = results.iterator();
 
-        AmazonClientException lastException = null;
-
         if (resultsIterator.hasNext()) {
             final Product item = resultsIterator.next();
 
             Product originalProduct = new Product(item);
-            //Product originalProduct = item;
 
             item.setName(product.getName());
             item.setDescription(product.getDescription());
             item.setCost(product.getCost());
-            // Demonstrate editing a single item.
+
             try {
                 mapper.save(item);
-
                 return originalProduct;
             } catch (final AmazonClientException ex) {
                 Log.e(LOG_TAG, "Failed editing item : " + ex.getMessage(), ex);
@@ -159,20 +142,9 @@ public class ProductTable extends NoSQLTableBase
 
         Iterator<Product> resultsIterator = results.iterator();
 
-        AmazonClientException lastException = null;
-
         final List<ITableObject> batchOfItems = new LinkedList<ITableObject>();
         while (resultsIterator.hasNext()) {
-            // Build a batch of books to delete.
-            for (int index = 0; index < MAX_BATCH_SIZE_FOR_DELETE && resultsIterator.hasNext(); index++) {
-                batchOfItems.add(resultsIterator.next());
-            }
-        }
-
-        if (lastException != null) {
-            // Re-throw the last exception encountered to alert the user.
-            // The logs contain all the exceptions that occurred during attempted delete.
-            throw lastException;
+            batchOfItems.add(resultsIterator.next());
         }
 
         ArrayList<ITableObject> n = new ArrayList<ITableObject>();
